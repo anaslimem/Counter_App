b8680230350f36aa35192695ffe38397eef5f85c
Anas Limem <160512789+anaslimem@users.noreply.github.com>
2024-05-26 12:48:40 +0100

    First commit

---
+++ Test.js
@@ -0,0 +1,25 @@
+let saveEl = document.getElementById("save-el")
+let counter = document.getElementById('counter')
+console.log(counter)
+console.log(saveEl)
+let count = 0
+function increment(){
+    count+=1
+    counter.textContent = count
+}
+function decrement(){
+    count-=1
+    counter.textContent = count
+}
+function save(){
+    let countNum=count + " - "
+    saveEl.textContent += countNum
+    counter.textContent = 0
+    count = 0
+}
+
+let message = "you have three new notifications now "
+console.log(message)
+
+
+
